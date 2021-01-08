76efd47fc227c1aa36bc516a7b307f2f71c23c9b
Nat Withe <nat@withnat.com>
2021-01-08 23:18:28 +0700

    Use ENV instead of CONSTANT for unit test friendly.

--- system/common.php
+++ system/common.php
@@ -219,7 +219,11 @@ if (!function_exists('isSPA'))
 	 */
 	function isSPA() : bool
 	{
-		if ((SIDE === 'frontend' and FRONTEND_SPA_MODE) or (SIDE === 'backend' and BACKEND_SPA_MODE))
+		$side = getenv('SIDE');
+		$frontendSpaMode = getenv('FRONTEND_SPA_MODE');
+		$backendSpaMode = getenv('BACKEND_SPA_MODE');
+
+		if (($side === 'frontend' and $frontendSpaMode) or ($side === 'backend' and $backendSpaMode))
 			return true;
 		else
 			return false;
