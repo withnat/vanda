0e25c620b309ebaa158e262719456e67da608033
Nat Withe <nat@withnat.com>
2021-01-15 10:48:37 +0700

    Add RFC info to HTTP status codes.

--- system/core/Response.php
+++ system/core/Response.php
@@ -55,64 +55,65 @@ class Response
 	 *
 	 * @var array  An array of standard HTTP status codes and reason phrases.
 	 * @link http://www.iana.org/assignments/http-status-codes/http-status-codes.xhtml
+     * @link https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
 	 */
 	protected static $_statuses = [
 		// 1xx: Informational
 		100 => 'Continue',
 		101 => 'Switching Protocols',
-		102 => 'Processing',
-		103 => 'Early Hints',
+		102 => 'Processing', // WebDAV; RFC 2518
+		103 => 'Early Hints', // RFC 8297
 		// 2xx: Success
 		200 => 'OK',
 		201 => 'Created',
 		202 => 'Accepted',
-		203 => 'Non-Authoritative Information',
+		203 => 'Non-Authoritative Information', // since HTTP/1.1
 		204 => 'No Content',
 		205 => 'Reset Content',
-		206 => 'Partial Content',
-		207 => 'Multi-Status',
-		208 => 'Already Reported',
-		226 => 'IM Used',
+		206 => 'Partial Content', // RFC 7233
+		207 => 'Multi-Status', // WebDAV; RFC 4918
+		208 => 'Already Reported', // WebDAV; RFC 5842
+		226 => 'IM Used', // RFC 3229
 		// 3xx: Redirection
 		300 => 'Multiple Choices',
 		301 => 'Moved Permanently',
 		302 => 'Found', // Moved Temporarily
-		303 => 'See Other',
-		304 => 'Not Modified',
-		305 => 'Use Proxy',
+		303 => 'See Other', // since HTTP/1.1
+		304 => 'Not Modified', // RFC 7232
+		305 => 'Use Proxy', // since HTTP/1.1
 		306 => 'Switch Proxy', // No longer used
-		307 => 'Temporary Redirect',
-		308 => 'Permanent Redirect',
+		307 => 'Temporary Redirect', // since HTTP/1.1
+		308 => 'Permanent Redirect', // RFC 7538
 		// 4xx: Client error
 		400 => 'Bad Request',
-		401 => 'Unauthorized',
+		401 => 'Unauthorized', // RFC 7235
 		402 => 'Payment Required',
 		403 => 'Forbidden',
 		404 => 'Not Found',
 		405 => 'Method Not Allowed',
 		406 => 'Not Acceptable',
-		407 => 'Proxy Authentication Required',
+		407 => 'Proxy Authentication Required', // RFC 7235
 		408 => 'Request Timeout',
 		409 => 'Conflict',
 		410 => 'Gone',
 		411 => 'Length Required',
-		412 => 'Precondition Failed',
-		413 => 'Request Entity Too Large',
-		414 => 'Request-URI Too Long',
-		415 => 'Unsupported Media Type',
-		416 => 'Requested Range Not Satisfiable',
+		412 => 'Precondition Failed', // RFC 7232
+		413 => 'Request Entity Too Large', // RFC 7231
+		414 => 'Request-URI Too Long', // RFC 7231
+		415 => 'Unsupported Media Type', // RFC 7231
+		416 => 'Requested Range Not Satisfiable', // RFC 7233
 		417 => 'Expectation Failed',
-		418 => 'I\'m a Teapot',
-		421 => 'Misdirected Request',
-		422 => 'Unprocessable Entity',
-		423 => 'Locked',
-		424 => 'Failed Dependency',
-		425 => 'Too Early',
+		418 => 'I\'m a Teapot', // RFC 2324, RFC 7168
+		421 => 'Misdirected Request', // RFC 7540
+		422 => 'Unprocessable Entity', // WebDAV; RFC 4918
+		423 => 'Locked', // WebDAV; RFC 4918
+		424 => 'Failed Dependency', // WebDAV; RFC 4918
+		425 => 'Too Early', // RFC 8470
 		426 => 'Upgrade Required',
-		428 => 'Precondition Required',
-		429 => 'Too Many Requests',
-		431 => 'Request Header Fields Too Large',
-		451 => 'Unavailable For Legal Reasons',
+		428 => 'Precondition Required', // RFC 6585
+		429 => 'Too Many Requests', // RFC 6585
+		431 => 'Request Header Fields Too Large', // RFC 6585
+		451 => 'Unavailable For Legal Reasons', // RFC 7725
 		// 5xx: Server error
 		500 => 'Internal Server Error',
 		501 => 'Not Implemented',
@@ -120,12 +121,12 @@ class Response
 		503 => 'Service Unavailable',
 		504 => 'Gateway Timeout',
 		505 => 'HTTP Version Not Supported',
-		506 => 'Variant Also Negotiates',
-		507 => 'Insufficient Storage',
-		508 => 'Loop Detected',
+		506 => 'Variant Also Negotiates', // RFC 2295
+		507 => 'Insufficient Storage', // WebDAV; RFC 4918
+		508 => 'Loop Detected', // WebDAV; RFC 5842
 		509 => 'Bandwidth Limit Exceeded',
-		510 => 'Not Extended',
-		511 => 'Network Authentication Required'
+		510 => 'Not Extended', // RFC 2774
+		511 => 'Network Authentication Required' // RFC 6585
 	];
 
 	/**
