157af46a49c49f2cfd98bab6a2e3058d4a454d67
Nat Withe <nat@withnat.com>
2021-01-30 16:18:10 +0700

    Update document for Str::replaceLast() method.

--- system/core/Str.php
+++ system/core/Str.php
@@ -1285,11 +1285,21 @@ final class Str
 	/**
 	 * Replaces the last occurrence of a given value in the string.
 	 *
-	 * @param  string      $string
-	 * @param  string      $search
-	 * @param  string      $replace
-	 * @param  string|null $encoding
-	 * @return string
+	 * For example,
+	 *
+	 * ```php
+	 * $string = 'ABCDEF:eFMNRZa:/fabcdefa:Bmnrz';
+	 *
+	 * $result = Str::replaceLast(':', '|');
+	 * // the result is: ABCDEF:eFMNRZa:/fabcdefa|Bmnrz
+	 * ```
+	 *
+	 * @param  string      $string    The string being searched and replaced on.
+	 * @param  string      $search    The value being searched for.
+	 * @param  string      $replace   The replacement value that replaces found search value.
+	 * @param  string|null $encoding  Optionally, the character encoding. If it is omitted or null, the internal
+	 *                                character encoding value will be used.
+	 * @return string                 Returns a string with the replaced value.
 	 */
 	public static function replaceLast(string $string, string $search, string $replace, string $encoding = null) : string
 	{
