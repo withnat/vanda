49a6e964f60c26d577dad7cc24e435eed170aa0a
Nat Withe <nat@withnat.com>
2021-02-02 15:14:38 +0700

    Update document for Hash class.

--- system/core/Hash.php
+++ system/core/Hash.php
@@ -26,23 +26,23 @@ final class Hash
 	private function __construct(){}
 
 	/**
-	 * Creates a new hash
+	 * Creates a new hash from the given string.
 	 *
-	 * @param  string      $value   String to hash.
-	 * @param  int         $rounds  Hashing rounds to apply (optional).
-	 * @return string|bool          Returns the hashed password, or FALSE on failure.
+	 * @param  string      $string  The input string to hash.
+	 * @param  int         $rounds  Optional, hashing rounds to apply. Defaults to 10.
+	 * @return string|bool          Returns the hashed string, or false on failure.
 	 */
-	public static function make(string $value, int $rounds = 10)
+	public static function make(string $string, int $rounds = 10)
 	{
-		return password_hash($value, PASSWORD_DEFAULT, ['cost' => $rounds]);
+		return password_hash($string, PASSWORD_DEFAULT, ['cost' => $rounds]);
 	}
 
 	/**
-	 * Verifies a hash
+	 * Verifies a hash.
 	 *
-	 * @param  string $value  Value to verify the hash against.
-	 * @param  string $hash   Hash to check.
-	 * @return bool           Whether the hash is valud.
+	 * @param  string $value  The input value to verify the hash against.
+	 * @param  string $hash   The hash to check.
+	 * @return bool           Returns true if the hash is valud, false otherwise.
 	 */
 	public static function verify(string $value, string $hash) : bool
 	{
