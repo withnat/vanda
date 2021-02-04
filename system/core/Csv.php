3ead0c0bba787ecbbe0fc8f258be9cb7e06a106a
Nat Withe <nat@withnat.com>
2021-02-04 20:24:16 +0700

    Update document for CSV class.

--- system/core/Csv.php
+++ system/core/Csv.php
@@ -33,18 +33,15 @@ final class Csv
 	private function __construct(){}
 
 	/**
-	 * @param  array  $dataset
-	 * @param  string $delimiter
-	 * @param  string $newline
-	 * @param  string $enclosure
-	 * @return string
+	 * Generates CSV string from the given dataset (array of arrays).
+	 *
+	 * @param  array  $dataset    The input dataset (array of arrays).
+	 * @param  string $delimiter  Optionally, delimiter. Defaults to , (comma).
+	 * @param  string $newline    Optionally, newline character. Default to \n
+	 * @param  string $enclosure  Optionally, enclosure. Default to " (double quote).
+	 * @return string             Returns the well-formed CSV.
 	 */
-	public static function fromDataset(
-		array  $dataset,
-		string $delimiter = ',',
-		string $newline = "\n",
-		string $enclosure = '"'
-	) : string
+	public static function fromDataset(array  $dataset, string $delimiter = ',', string $newline = "\n", string $enclosure = '"') : string
 	{
 		if (!Arr::isDataset($dataset))
 			throw InvalidArgumentException::typeError(1, ['dataset'], $dataset);
@@ -55,18 +52,15 @@ final class Csv
 	}
 
 	/**
-	 * @param  array  $recordset
-	 * @param  string $delimiter
-	 * @param  string $newline
-	 * @param  string $enclosure
-	 * @return string
+	 * Generates CSV string from the given recordset (array of objects).
+	 *
+	 * @param  array  $recordset  The input recordset (array of objects).
+	 * @param  string $delimiter  Optionally, delimiter. Defaults to , (comma).
+	 * @param  string $newline    Optionally, newline character. Default to \n
+	 * @param  string $enclosure  Optionally, enclosure. Default to " (double quote).
+	 * @return string             Returns the well-formed CSV.
 	 */
-	public static function fromRecordset(
-		array  $recordset,
-		string $delimiter = ',',
-		string $newline = "\n",
-		string $enclosure = '"'
-	) : string
+	public static function fromRecordset(array  $recordset, string $delimiter = ',', string $newline = "\n", string $enclosure = '"') : string
 	{
 		if (!Arr::isRecordset($recordset))
 			throw InvalidArgumentException::typeError(1, ['recordset'], $recordset);
@@ -77,18 +71,15 @@ final class Csv
 	}
 
 	/**
-	 * @param  string $csv
-	 * @param  string $delimiter
-	 * @param  string $newline
-	 * @param  string $enclosure
+	 * Converts the given CSV string to an array.
+	 *
+	 * @param  string $csv        The well-formed CSV string.
+	 * @param  string $delimiter  Optionally, delimiter. Defaults to , (comma).
+	 * @param  string $newline    Optionally, new line character. Defaults to \n
+	 * @param  string $enclosure  Optionally, enclosure. Defaults to " (double quote).
 	 * @return array
 	 */
-	public static function toArray(
-		string $csv,
-		string $delimiter = ',',
-		string $newline = "\n",
-		string $enclosure = '"'
-	) : array
+	public static function toArray(string $csv, string $delimiter = ',', string $newline = "\n", string $enclosure = '"') : array
 	{
 		$lines = explode($newline, $csv);
 		$data = [];
@@ -106,18 +97,15 @@ final class Csv
 	}
 
 	/**
-	 * @param  string $csv
-	 * @param  string $delimiter
-	 * @param  string $newline
-	 * @param  string $enclosure
-	 * @return array
+	 * Converts the given CSV string to dataset (array of arrays).
+	 *
+	 * @param  string $csv        The well-formed CSV string.
+	 * @param  string $delimiter  Optionally, delimiter. Defaults to , (comma).
+	 * @param  string $newline    Optionally, new line character. Defaults to \n
+	 * @param  string $enclosure  Optionally, enclosure. Defaults to " (double quote).
+	 * @return array              Returns dataset (array of arrays).
 	 */
-	public static function toDataset(
-		string $csv,
-		string $delimiter = ',',
-		string $newline = "\n",
-		string $enclosure = '"'
-	) : array
+	public static function toDataset(string $csv, string $delimiter = ',', string $newline = "\n", string $enclosure = '"') : array
 	{
 		$lines = explode($newline, $csv);
 		$header = null;
@@ -140,18 +128,15 @@ final class Csv
 	}
 
 	/**
-	 * @param  string $csv
-	 * @param  string $delimiter
-	 * @param  string $newline
-	 * @param  string $enclosure
-	 * @return object
+	 * Converts the given CSV string to recordset (array of objects).
+	 *
+	 * @param  string $csv        The well-formed CSV string.
+	 * @param  string $delimiter  Optionally, delimiter. Defaults to , (comma).
+	 * @param  string $newline    Optionally, new line character. Defaults to \n
+	 * @param  string $enclosure  Optionally, enclosure. Defaults to " (double quote).
+	 * @return object             Returns recordset (array of objects).
 	 */
-	public static function toRecordset(
-		string $csv,
-		string $delimiter = ',',
-		string $newline = "\n",
-		string $enclosure = '"'
-	) : object
+	public static function toRecordset(string $csv, string $delimiter = ',', string $newline = "\n", string $enclosure = '"') : object
 	{
 		$array = Csv::toDataset($csv, $delimiter, $newline, $enclosure);
 		$object = Arr::toObject($array);
@@ -160,14 +145,13 @@ final class Csv
 	}
 
 	/**
-	 * @param  mixed  $string
-	 * @param  string $enclosure
-	 * @return string
+	 * Escapes enclosure character from the given string.
+	 *
+	 * @param  mixed  $string     The input string.
+	 * @param  string $enclosure  The enclosure character.
+	 * @return string             Returns the string without enclosure character.
 	 */
-	public static function safe(
-		$string,
-		string $enclosure
-	) : string
+	public static function safe($string, string $enclosure) : string
 	{
 		if (is_string($string))
 			$string = str_replace($enclosure, $enclosure . $enclosure, $string);
@@ -176,16 +160,14 @@ final class Csv
 	}
 
 	/**
-	 * @param  string $line
-	 * @param  string $delimiter
-	 * @param  string $enclosure
-	 * @return array
+	 * Parses the given CSV string and returns in array.
+	 *
+	 * @param  string $line       The CSV string.
+	 * @param  string $delimiter  The delimiter character.
+	 * @param  string $enclosure  The enclosure character.
+	 * @return array              Returns an array.
 	 */
-	private static function _parseLine(
-		string $line,
-		string $delimiter,
-		string $enclosure
-	) : array
+	private static function _parseLine(string $line, string $delimiter, string $enclosure) : array
 	{
 		$columns = explode($delimiter, $line);
 
@@ -201,12 +183,16 @@ final class Csv
 		return $columns;
 	}
 
-	private static function _fromDatasetOrRecordset(
-		array  $datasetOrRecordset,
-		string $delimiter = ',',
-		string $newline = "\n",
-		string $enclosure = '"'
-	) : string
+	/**
+	 * Generates CSV from the given dataset (array of arrays) or recordset (array of objects).
+	 *
+	 * @param array  $datasetOrRecordset  The input dataset (array of arrays) or recordset (array of objects).
+	 * @param string $delimiter           Delimiter.
+	 * @param string $newline             Newline character.
+	 * @param string $enclosure           Enclosure.
+	 * @return string                     Returns the well-formed CSV.
+	 */
+	private static function _fromDatasetOrRecordset(array  $datasetOrRecordset, string $delimiter, string $newline, string $enclosure) : string
 	{
 		$header = '';
 		$csv = '';
