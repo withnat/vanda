1774ddcb91e5d09507b6a4656361c17667c6ad58
Nat Withe <nat@withnat.com>
2023-09-29 20:16:51 +0700

    Add unit test cases for Flash::warning method.

--- tests/unit/FlashTest.php
+++ tests/unit/FlashTest.php
@@ -48,6 +48,7 @@ class FlashTest extends TestCase
 				. '<button aria-hidden="true" data-dismiss="alert" type="button" class="dummy-button-class">×</button>'
 				. 'test'
 				. '</div>';
+
 		$result = Flash::info('test');
 
 		$this->assertEquals($expected, $result);
@@ -90,6 +91,7 @@ class FlashTest extends TestCase
 			. '<button aria-hidden="true" data-dismiss="alert" type="button" class="dummy-button-class">×</button>'
 			. 'test'
 			. '</div>';
+
 		$result = Flash::success('test');
 
 		$this->assertEquals($expected, $result);
@@ -108,6 +110,49 @@ class FlashTest extends TestCase
 		$mockedSession->shouldReceive('set')->once();
 
 		Flash::success('test');
+
+		// If this test fails, it will stop before returning true below.
+		$this->assertTrue(true);
+	}
+
+	// Flash::warning()
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function testMethodWarningCase1() : void
+	{
+		$stubRequest = Mockery::mock('alias:\System\Request');
+		$stubRequest->shouldReceive('isAjax')->andReturnTrue();
+
+		$stubConfig = Mockery::mock('alias:\System\Config');
+		$stubConfig->shouldReceive('core')->with('closeFlashWarningMessageWrapperClass')->andReturn('dummy-wrapper-class');
+		$stubConfig->shouldReceive('core')->with('closeFlashWarningMessageButtonClass')->andReturn('dummy-button-class');
+
+		$expected = '<div class="dummy-wrapper-class">'
+			. '<button aria-hidden="true" data-dismiss="alert" type="button" class="dummy-button-class">×</button>'
+			. 'test'
+			. '</div>';
+
+		$result = Flash::warning('test');
+
+		$this->assertEquals($expected, $result);
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function testMethodWarningCase2() : void
+	{
+		$stubRequest = Mockery::mock('alias:\System\Request');
+		$stubRequest->shouldReceive('isAjax')->andReturnFalse();
+
+		$mockedSession = Mockery::mock('alias:\System\Session');
+		$mockedSession->shouldReceive('set')->once();
+
+		Flash::warning('test');
 
 		// If this test fails, it will stop before returning true below.
 		$this->assertTrue(true);
