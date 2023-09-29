1e4f2df500310275a110216dae627ed00a3fc2f9
Nat Withe <nat@withnat.com>
2023-09-29 20:12:24 +0700

    Add unit test cases for Flass::info() method.

---
+++ tests/unit/FlashTest.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Vanda
+ *
+ * A lightweight & flexible PHP CMS framework
+ *
+ * @package     Vanda
+ * @author      Nat Withe <nat@withnat.com>
+ * @copyright   Copyright (c) 2010 - 2021, Nat Withe. All rights reserved.
+ * @link        http://vanda.io
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use Mockery;
+use PHPUnit\Framework\TestCase;
+use System\Flash;
+
+/**
+ * Class ConfigTest
+ * @package Tests\Unit
+ */
+class FlashTest extends TestCase
+{
+	protected function tearDown() : void
+	{
+		Mockery::close();
+	}
+
+	// Flash::info()
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function testMethodInfoCase1() : void
+	{
+		$stubRequest = Mockery::mock('alias:\System\Request');
+		$stubRequest->shouldReceive('isAjax')->andReturnTrue();
+
+		$stubConfig = Mockery::mock('alias:\System\Config');
+		$stubConfig->shouldReceive('core')->with('closeFlashInfoMessageWrapperClass')->andReturn('dummy-wrapper-class');
+		$stubConfig->shouldReceive('core')->with('closeFlashInfoMessageButtonClass')->andReturn('dummy-button-class');
+
+		$expected = '<div class="dummy-wrapper-class">'
+				. '<button aria-hidden="true" data-dismiss="alert" type="button" class="dummy-button-class">×</button>'
+				. 'test'
+				. '</div>';
+		$result = Flash::info('test');
+
+		$this->assertEquals($expected, $result);
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function testMethodInfoCase2() : void
+	{
+		$stubRequest = Mockery::mock('alias:\System\Request');
+		$stubRequest->shouldReceive('isAjax')->andReturnFalse();
+
+		$mockedSession = Mockery::mock('alias:\System\Session');
+		$mockedSession->shouldReceive('set')->once();
+
+		Flash::info('test');
+
+		// If this test fails, it will stop before returning true below.
+		$this->assertTrue(true);
+	}
+}
