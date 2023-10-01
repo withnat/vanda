37f0abe7e9428000883f9f727fa0ab8ada503daf
Nat Withe <nat@withnat.com>
2023-10-01 15:32:29 +0700

    Add comments and update Cookie class.

--- system/core/Cookie.php
+++ system/core/Cookie.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 
 namespace System;
 
+use ErrorException;
 use System\Exception\InvalidArgumentException;
 
 /**
@@ -43,9 +44,16 @@ class Cookie
 	public static function set(string $name, $value = null, int $expire = 0) : void
 	{
 		if (is_resource($value))
-			throw InvalidArgumentException::typeError(2, ['string', 'int', 'float', 'array', 'object', 'null'], $default);
+			throw InvalidArgumentException::typeError(2, ['string', 'int', 'float', 'array', 'object', 'null'], $value);
 
-		if (is_array($value) or is_object($value))
+		if (is_array($value))
+			$value = ['datatype' => 'array', 'value' => $value];
+		elseif (is_object($value))
+			$value = ['datatype' => 'object', 'value' => $value];
+		elseif (is_bool($value))
+			$value = ['datatype' => 'bool', 'value' => $value];
+
+		if (is_array($value))
 			$value = json_encode($value);
 
 		// PHP 7.2 : setcookie() function does not have SameSite attribute.
@@ -76,26 +84,78 @@ class Cookie
 		}
 	}
 
-	public static function get($name, $default = null)
+	/**
+	 * This method provides a friendlier syntax for getting browser cookies.
+	 *
+	 * @param string $name     The name of the cookie.
+	 * @param mixed  $default  The default value to return if the cookie does not exist. Defaults to null.
+	 * @return mixed           Returns the value of the cookie if it exists, otherwise returns the default value.
+	 * @throws ErrorException
+	 */
+	public static function get(string $name, $default = null)
 	{
 		$value = '';
 
 		if (isset($_COOKIE[$name]))
 			$value = $_COOKIE[$name];
 
-		if ($value == '')
+		if (mb_strlen((string)$value) === 0)
 			$value = $default;
+
+		if (Json::isValid($value))
+		{
+			$value = Json::decode($value, true);
+
+			if (isset($value['datatype']))
+			{
+				switch ($value['datatype'])
+				{
+					case 'array':
+						$value = $value['value'];
+						break;
+					case 'object':
+						$value = (object)$value['value'];
+						break;
+					case 'bool':
+						$value = (bool)$value['value'];
+						break;
+				}
+			}
+		}
 
 		return $value;
 	}
 
-	public static function has($name) : bool
+	/**
+	 * This method returns true if a cookie exists, false otherwise.
+	 *
+	 * @param string $name  The name of the cookie to check.
+	 * @return bool
+	 */
+	public static function has(string $name) : bool
 	{
 		return isset($_COOKIE[$name]);
 	}
 
-	public static function clear($name)
+	/**
+	 * This method deletes a cookie by setting its expiration date to the past.
+	 *
+	 * @param string $name  The name of the cookie to delete.
+	 * @return void
+	 */
+	public static function delete(string $name) : void
 	{
 		setcookie($name, '', time()-3600, '/');
+	}
+
+	/**
+	 * This method clears all cookies.
+	 *
+	 * @return void
+	 */
+	public static function clear() : void
+	{
+		foreach ($_COOKIE as $name => $value)
+			setcookie($name, '', time()-3600, '/');
 	}
 }
