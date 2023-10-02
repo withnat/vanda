6e7ff7493f16a2980370b57e6d7cabdf1d498815
Nat Withe <nat@withnat.com>
2023-10-02 10:34:58 +0700

    Add unit test cases for Cookie::set() method.

--- tests/unit/CookieTest.php
+++ tests/unit/CookieTest.php
@@ -14,8 +14,10 @@ declare(strict_types=1);
 
 namespace Tests\Unit;
 
+use InvalidArgumentException;
 use Mockery;
 use PHPUnit\Framework\TestCase;
+use stdClass;
 use System\Cookie;
 
 /**
@@ -27,5 +29,80 @@ class CookieTest extends TestCase
 	protected function tearDown() : void
 	{
 		Mockery::close();
+	}
+
+	// Cookie::set()
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function testMethodSetCase1() : void
+	{
+		$this->expectException(InvalidArgumentException::class);
+
+		Cookie::set('name', tmpfile());
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function testMethodSetCase2() : void
+	{
+		Cookie::set('name', 'value');
+
+		$this->assertTrue(true);
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function testMethodSetCase3() : void
+	{
+		Cookie::set('name', ['foo' => 'bar']);
+
+		$this->assertTrue(true);
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function testMethodSetCase4() : void
+	{
+		$value = new stdClass();
+		$value->foo = 'bar';
+
+		Cookie::set('name', $value);
+
+		$this->assertTrue(true);
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function testMethodSetCase5() : void
+	{
+		Cookie::set('name', true);
+
+		$this->assertTrue(true);
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function testMethodSetCase6() : void
+	{
+		$_SERVER['SERVER_PORT'] = 443;
+
+		Cookie::set('name', 'value');
+
+		$this->assertTrue(true);
+
+		unset($_SERVER['SERVER_PORT']);
 	}
 }
