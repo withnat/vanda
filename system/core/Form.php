eb18b40a3494bb32dd2b4a07b1bf283022ec704e
Nat Withe <nat@withnat.com>
2023-10-02 22:03:01 +0700

    Add Form class to the project.

---
+++ system/core/Form.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Vanda
+ *
+ * A lightweight & flexible PHP CMS framework
+ *
+ * @package     Vanda
+ * @author      Nat Withe <nat@withnat.com>
+ * @copyright   Copyright (c) 2010 - 2021, Nat Withe. All rights reserved.
+ * @link        http://vanda.io
+ */
+
+declare(strict_types=1);
+
+namespace System;
+
+/**
+ * Class Form
+ *
+ * The Form class manages form functionality for the framework, allowing
+ * easy creation, validation, and processing of HTML forms.
+ *
+ * @package System
+ */
+class Form
+{
+}
