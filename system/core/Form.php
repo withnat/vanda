4f0d3c1d668a26940a1b3a57b76fe5b4d0fee080
Nat Withe <nat@withnat.com>
2023-10-03 20:56:56 +0700

    Added Form::open() method to Form class.

--- system/core/Form.php
+++ system/core/Form.php
@@ -24,4 +24,52 @@ namespace System;
  */
 class Form
 {
+	protected static $_formId;
+
+	/**
+	 * Creates a form open tag with the necessary attributes.
+	 *
+	 * @param string|null       $action   The URL or package action to submit the form to. Defaults to null.
+	 *                                    If null, the form will submit to the current package's saveAction().
+	 * @param string|array|null $attribs  Additional attributes for the form tag. Defaults to null.
+	 * @return string                     Returns the form open tag.
+	 */
+	public static function open(?string $action = null, $attribs = null) : string
+	{
+		$action = trim((string)$action);
+
+		if (stripos($action, 'http://') === false and stripos($action, 'https://') === false)
+		{
+			if (!$action)
+				$action = 'save';
+
+			$action = Url::createFromAction($action);
+		}
+
+		$id = Html::getAttribute($attribs, 'id');
+		$name = Html::getAttribute($attribs, 'name');
+
+		if (!$id and !$name)
+		{
+			$id = 'frm-default';
+			$name = 'frm-default';
+		}
+		elseif ($id)
+			$name = $id;
+		elseif ($name)
+			$id = $name;
+
+		static::$_formId = $id;
+
+		$attribs = Html::setAttribute($attribs, 'id', $id);
+		$attribs = Html::setAttribute($attribs, 'name', $name);
+		$attribs = Html::setAttribute($attribs, 'action', $action);
+		$attribs = Html::setAttribute($attribs, 'method', 'post');
+		$attribs = Html::setAttribute($attribs, 'class', 'form-horizontal form');
+		$attribs = Html::setAttribute($attribs, 'enctype', 'multipart/form-data');
+
+		$html = '<form ' . $attribs . '>';
+
+		return $html;
+	}
 }
