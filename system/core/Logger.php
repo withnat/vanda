dac59f55926004f8fa62050908eb74693f2256ac
Nat Withe <nat@withnat.com>
2023-10-13 11:13:37 +0700

    Added Logger::alert() method.

--- system/core/Logger.php
+++ system/core/Logger.php
@@ -92,6 +92,17 @@ class Logger
 	}
 
 	/**
+	 * Writes an alert message to the log file.
+	 *
+	 * @param  string $message  The message to write.
+	 * @return bool             Returns true if the message was written to the log file, false otherwise.
+	 */
+	public static function alert(string $message) : bool
+	{
+		return static::log('alert', $message);
+	}
+
+	/**
 	 * Writes a message to the log file.
 	 *
 	 * @param  string $level    The log level.
