df2d07309a2b4ff5d3a784532c58eaffcd9ed2f8
Nat Withe <nat@withnat.com>
2023-10-13 11:22:48 +0700

    Added Logger::notice() method.

--- system/core/Logger.php
+++ system/core/Logger.php
@@ -136,6 +136,17 @@ class Logger
 	}
 
 	/**
+	 * Writes a notice message to the log file.
+	 *
+	 * @param  string $message  The message to write.
+	 * @return bool             Returns true if the message was written to the log file, false otherwise.
+	 */
+	public static function notice(string $message) : bool
+	{
+		return static::log('notice', $message);
+	}
+
+	/**
 	 * Writes a message to the log file.
 	 *
 	 * @param  string $level    The log level.
