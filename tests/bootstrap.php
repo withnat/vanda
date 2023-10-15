39552bc2a60639eeb782d7e1bbc082f991321d53
Nat Withe <nat@withnat.com>
2023-10-15 22:10:19 +0700

    Updated PHPUnit bootstrap file.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -16,23 +16,29 @@ namespace Tests;
 
 use System\Autoloader;
 
-// Set constants used in Vanda.
+// Define a constant used within the framework.
+// Cannot use the 'const' keyword in this bootstrap file because
+// it is in a different namespace from the core files.
+
 define('VD', 1);
-define('ENV', 'development'); // 'development', 'testing', 'staging' or 'production'
+
 define('DS', DIRECTORY_SEPARATOR);
-define('PATH_BASE', __DIR__ . DS . '..');
-define('PATH_APP', PATH_BASE . DS . 'app');
-define('PATH_CONFIG', PATH_BASE . DS . 'config');
-define('PATH_STORAGE', PATH_BASE . DS . 'storage');
-define('PATH_SYSTEM', PATH_BASE . DS . 'system');
-define('PATH_THEMES', PATH_BASE . DS . 'themes');
-define('PATH_VENDOR', PATH_BASE . DS . 'vendor');
+define('PATH_BASE',  __DIR__ . DS . '..');
+define('PATH_APP',  PATH_BASE . DS . 'app');
+define('PATH_CONFIG',  PATH_BASE . DS . 'config');
+define('PATH_STORAGE',  PATH_BASE . DS . 'storage');
+define('PATH_SYSTEM',  PATH_BASE . DS . 'system');
+define('PATH_THEME',  PATH_BASE . DS . 'themes');
+define('PATH_VENDOR',  PATH_BASE . DS . 'vendor');
+define('ENVIRONMENT',  'development'); // 'development', 'testing', 'staging' or 'production'
 
 // Load the Vanda autoloader.
+
 require PATH_SYSTEM . '/Autoloader.php';
 require PATH_SYSTEM . '/common.php';
 
 // Create the Composer autoloader.
+
 $loader = require PATH_VENDOR . '/autoload.php';
 $loader->unregister();
 
