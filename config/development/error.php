2a47742278f429563bfdc38fee49b6bc5f98882e
Nat Withe <nat@withnat.com>
2023-10-15 23:21:17 +0700

    Updated error config file.

--- config/development/error.php
+++ config/development/error.php
@@ -12,22 +12,5 @@
 
 return [
 	'report' => true,
-	'log' => function($e)
-	{
-		$data = [
-			'date' => date('Y-m-d H:i:s'),
-			'message' => $e->getMessage(),
-			'file' => $e->getFile(),
-			'line' => $e->getLine(),
-			'trace' => $e->getTrace()
-		];
-
-		$path = PATH_STORAGE . DS . 'logs' . DS . 'errors.log';
-
-		// Use a JSON library instead of json_encode()
-		// to handle data that cannot be encoded.
-		$content = System\Json::encode($data) . "\n";
-
-		file_put_contents($path, $content, FILE_APPEND | LOCK_EX);
-	}
+	'log' => true
 ];
