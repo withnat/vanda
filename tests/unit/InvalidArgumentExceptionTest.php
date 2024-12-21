5bbf8113bcdd7809f646f23f86619d41ee40aa8d
Nat Withe <nat@withnat.com>
2024-12-21 09:07:48 +0700

    Added unit test for InvalidArgumentException class.

---
+++ tests/unit/InvalidArgumentExceptionTest.php
@@ -0,0 +1,58 @@
+<?php
+/*
+ * __      __             _
+ * \ \    / /            | |
+ *  \ \  / /_ _ _ __   __| | __ _
+ *   \ \/ / _` | '_ \ / _` |/ _` |
+ *    \  / (_| | | | | (_| | (_| |
+ *     \/ \__,_|_| |_|\__,_|\__,_|
+ *
+ * Vanda
+ *
+ * A lightweight & flexible PHP web framework
+ *
+ * @package      Vanda
+ * @author       Nat Withe <nat@withnat.com>
+ * @copyright    Copyright (c) 2010 - 2023, Nat Withe. All rights reserved.
+ * @link         https://vanda.io
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use System\Arr;
+use System\Exception\InvalidArgumentException;
+
+/**
+ * Class InvalidArgumentExceptionTest
+ * @package Tests\Unit
+ */
+class InvalidArgumentExceptionTest extends TestCase
+{
+	// InvalidArgumentException::typeError()
+
+	public function testMethodTypeErrorCase1() : void
+	{
+		$exception = InvalidArgumentException::typeError(1, ['string', 'int', 'float', 'bool', 'null'], tmpfile());
+
+		$this->assertInstanceOf(InvalidArgumentException::class, $exception);
+	}
+
+	public function testMethodTypeErrorCase2() : void
+	{
+		$exception = InvalidArgumentException::typeError(1, ['string', 'int', 'float', 'bool', 'null'], tmpfile(), 'custom message');
+
+		$this->assertInstanceOf(InvalidArgumentException::class, $exception);
+	}
+
+	// InvalidArgumentException::valueError()
+
+	public function testMethodValueErrorCase1() : void
+	{
+		$exception = InvalidArgumentException::valueError(1, '$value must be greater than zero', false);
+
+		$this->assertInstanceOf(InvalidArgumentException::class, $exception);
+	}
+}
